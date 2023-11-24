7b184cf6c554e7269d7c3653d0e9f43c0f68b53a
JACE <jacegem@gmail.com>
2023-11-24 10:17:18 +0900

    add raw config

--- src/main.clj
+++ src/main.clj
@@ -16,30 +16,29 @@
                              :mods ["left_option"]}
                         :alone-key "escapse"}
 
-
-   ;; double
+   ;; raw
 ;; https://karabiner-elements.pqrs.org/docs/json/typical-complex-modifications-examples/#change-right_shift-x2-to-mission_control
-   #_#_double-right-shift {:rule-type :double
-                           :description "double-right-shift to show CLOCK"
-                           :manipulators [{:type "basic"
-                                           :from {:key_code  "right_shift"
-                                                  :modifiers {:optional ["any"]}}
-                                           :to [{:key_code "`"
-                                                 :modifiers ["left_control"
-                                                             "left_option"]}]
-                                           :conditions [{:type "variable_if"
-                                                         :name "right_shift pressed"
-                                                         :value 1}]}
-                                          {:type "basic"
-                                           :from {:key_code  "right_shift"
-                                                  :modifiers {:optional ["any"]}}
-                                           :to [{:set_variable {:name "right_shift pressed"
-                                                                :value 1}}
-                                                {:key_code "right_shift"}]
-                                           :to_delayed_action {:to_if_invoked [{:set_variable {:name "right_shift pressed"
-                                                                                               :value 0}}]
-                                                               :to_if_cancled [{:set_variable {:name "right_shift pressed"
-                                                                                               :value 0}}]}}]}
+   :double-right-shift {:rule-type :raw
+                        :description "double-right-shift to show CLOCK"
+                        :manipulators [{:type "basic"
+                                        :from {:key_code  "right_shift"
+                                               :modifiers {:optional ["any"]}}
+                                        :to [{:key_code "grave_accent_and_tilde"
+                                              :modifiers ["left_control"
+                                                          "left_option"]}]
+                                        :conditions [{:type "variable_if"
+                                                      :name "right_shift pressed"
+                                                      :value 1}]}
+                                       {:type "basic"
+                                        :from {:key_code  "right_shift"
+                                               :modifiers {:optional ["any"]}}
+                                        :to [{:set_variable {:name "right_shift pressed"
+                                                             :value 1}}
+                                             {:key_code "right_shift"}]
+                                        :to_delayed_action {:to_if_invoked [{:set_variable {:name "right_shift pressed"
+                                                                                            :value 0}}]
+                                                            :to_if_canceled [{:set_variable {:name "right_shift pressed"
+                                                                                             :value 0}}]}}]}
 
 
 ;;  https://karabiner-elements.pqrs.org/docs/json/typical-complex-modifications-examples/#change-equaldelete-to-forward_delete-if-these-keys-are-pressed-simultaneously
@@ -48,6 +47,7 @@
                                          :mods []}
                                   :to {:keys ["delete_forward"]
                                        :mods []}}
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
    ;; with A 
    :cmd+s {:from {:keys ["a" "s"]
                   :mods []}
@@ -62,7 +62,7 @@
    :cmd+f {:from {:keys ["a" "f"]
                   :mods []}
            :to {:keys ["f"]
-                :mods ["left_command"]}}
+                :mods ["left_command" "left_shift"]}}
 
    :cmd+x {:from {:keys ["a" "x"]
                   :mods []}
@@ -77,6 +77,11 @@
    :cmd+v {:from {:keys ["a" "v"]
                   :mods []}
            :to {:keys ["v"]
+                :mods ["left_command"]}}
+
+   :cmd+p {:from {:keys ["a" "p"]
+                  :mods []}
+           :to {:keys ["p"]
                 :mods ["left_command"]}}
 
    ;; row 1
@@ -246,11 +251,11 @@
 
 (defmulti rule-convert (fn [[_k v]] (:rule-type v)))
 
-(defmethod rule-convert :single [config]
-  (get-single-rule config))
+(defmethod rule-convert :raw [[k v]]
+  [(dissoc v :rule-type)])
 
-(defmethod rule-convert :double [config]
-  config)
+(defmethod rule-convert :single [config]
+  [(get-single-rule config)])
 
 (defmethod rule-convert :multi [config]
   [(get-simultaneous-rule config)
@@ -260,77 +265,109 @@
   [(get-simultaneous-rule config)
    (get-simultaneous-rule-right config)])
 
-
-(comment
-  (mapcat rule-convert config-map)
-  (rule-convert [:a {:rule-type :double}])
-
-  :rcf)
-
-(defn rule [config]
-  (let [[_ value] config
-        from-keys (get-in value [:from :keys])
-        keys-len (count from-keys)]
-    (if (<= keys-len 1)
-      (get-single-rule config)
-      (get-simultaneous-rule config))))
-
-(defn rule-right [config]
-  (let [[_name value] config
-        from-keys (get-in value [:from :keys])
-        keys-len (count from-keys)
-        right  (:right value)]
-    (when (and (>= keys-len 2)
-               (not= right false))
-      (get-simultaneous-rule-right config))))
-
-(defn generate [configs]
+(defn generate-raw [configs]
   (let [file-name "karabiner.json"
         karabiner (json/read-str (slurp file-name))
         profile (->> (get karabiner "profiles")
                      (filter #(= "Default profile" (get % "name")))
                      first)
-        new-rules (->> (concat (map rule configs)
-                               (map rule-right configs))
+        new-rules (->> (mapcat rule-convert configs)
                        (remove nil?))
         new-profile (assoc-in profile ["complex_modifications" "rules"] new-rules)
         json-str (-> (assoc karabiner "profiles" [new-profile])
                      (json/write-str {:escape-unicode false}))]
     (spit file-name json-str)))
 
-
 (comment
-  (generate config-map)
-  (def simple-map
-    {;; single 
-     :cap_lock->ctrl+opt {:from {:keys ["caps_lock"]
-                                 :mods []}
-                          :to {:keys ["left_control"]
-                               :mods ["left_option"]}}})
-  (generate simple-map)
-  @(def data (json/read-str (slurp "karabiner.json")))
-  @(def profiles (get data "profiles"))
-  @(def default-profile
-     (->> (filter #(= "Default profile" (get % "name"))
-                  profiles)
-          first))
-  @(def rules
-     (get-in default-profile ["complex_modifications" "rules"]))
+  (generate-raw config-map)
+  (mapcat rule-convert config-map)
+  (rule-convert [:a {:rule-type :double}])
 
-  @(def complex_modifications
-     (->> (filter #(= "complex_modifications" (first %))
-                  profiles)))
-
-  (keys (first profiles))
-  (map (fn [k]
-         (prn k)) profiles)
-
-  (-> data
-      (get "profiles")
-      #_(get "complex_modifications")
-      count)
-
-  (spit "z_data.json" (json/write-str data {:escape-unicode false}))
-  (spit "j.txt" (generate config-map))
-  (spit "rules.txt" (generate config-map))
   :rcf)
+
+;; (defn rule [config]
+;;   (let [[_ value] config
+;;         from-keys (get-in value [:from :keys])
+;;         keys-len (count from-keys)]
+;;     (if (<= keys-len 1)
+;;       (get-single-rule config)
+;;       (get-simultaneous-rule config))))
+
+;; (defn rule-right [config]
+;;   (let [[_name value] config
+;;         from-keys (get-in value [:from :keys])
+;;         keys-len (count from-keys)
+;;         right  (:right value)]
+;;     (when (and (>= keys-len 2)
+;;                (not= right false))
+;;       (get-simultaneous-rule-right config))))
+
+;; (defn generate [configs]
+;;   (let [file-name "karabiner.json"
+;;         karabiner (json/read-str (slurp file-name))
+;;         profile (->> (get karabiner "profiles")
+;;                      (filter #(= "Default profile" (get % "name")))
+;;                      first)
+;;         new-rules (->> (concat (map rule configs)
+;;                                (map rule-right configs))
+;;                        (remove nil?))
+;;         new-profile (assoc-in profile ["complex_modifications" "rules"] new-rules)
+;;         json-str (-> (assoc karabiner "profiles" [new-profile])
+;;                      (json/write-str {:escape-unicode false}))]
+;;     (spit file-name json-str)))
+
+
+;; (comment
+;;   (generate config-map)
+;;   (def simple-map
+;;     {;; single 
+;;      :cap_lock->ctrl+opt {:from {:keys ["caps_lock"]
+;;                                  :mods []}
+;;                           :to {:keys ["left_control"]
+;;                                :mods ["left_option"]}}})
+;;   (generate simple-map)
+;;   @(def data (json/read-str (slurp "karabiner.json")))
+;;   @(def profiles (get data "profiles"))
+;;   @(def default-profile
+;;      (->> (filter #(= "Default profile" (get % "name"))
+;;                   profiles)
+;;           first))
+;;   @(def rules
+;;      (get-in default-profile ["complex_modifications" "rules"]))
+
+;;   @(def complex_modifications
+;;      (->> (filter #(= "complex_modifications" (first %))
+;;                   profiles)))
+
+;;   (keys (first profiles))
+;;   (map (fn [k]
+;;          (prn k)) profiles)
+
+;;   (-> data
+;;       (get "profiles")
+;;       #_(get "complex_modifications")
+;;       count)
+
+;;   (spit "z_data.json" (json/write-str data {:escape-unicode false}))
+;;   (spit "j.txt" (generate config-map))
+;;   (spit "rules.txt" (generate config-map))
+;;   :rcf)
+;;    :a->cmd {:rule-type :raw
+;;             :description "a->cmd"
+;;             :manipulators [{:type "basic"
+;;                             :from {:key_code "a"
+;;                                    :modifiers {:optional ["any"]}}
+;;                             :parameters {:basic.to_if_alone_timeout_milliseconds 800
+;;                                          :basic.to_if_held_down_threshold_milliseconds 300}
+;;                             :to_if_alone [{:key_code "a"}]
+;;                             :to_if_held_down [{:key_code "left_command"}]}]}
+
+;;    :f->shift {:rule-type :raw
+;;               :description "f->cmd"
+;;               :manipulators [{:type "basic"
+;;                               :from {:key_code "f"
+;;                                      :modifiers {:optional ["any"]}}
+;;                               :parameters {:basic.to_if_alone_timeout_milliseconds 900
+;;                                            :basic.to_if_held_down_threshold_milliseconds 900}
+;;                               :to_if_alone [{:key_code "f"}]
+;;                               :to_if_held_down [{:key_code "left_shift"}]}]}
